d8097c0bc60a4a9f268dbd6245a3c785694343c4
krisk <krisk@krisks-MacBook-Pro.local>
2024-06-25 00:38:37 -0700

    changing to charbody

--- player.gd
+++ player.gd
@@ -1,7 +1,6 @@
 extends CharacterBody3D
 
 func _process(delta):
-	rotation.y += -Input.get_axis("left", "right")*0.02
-	velocity += -0.1*get_global_transform().basis.z*Input.get_axis("down", "up")
-	velocity *= 0.95
+	velocity += -transform.basis.z * Input.get_axis("down", "up")
+	velocity *= 0.8
 	move_and_slide()
