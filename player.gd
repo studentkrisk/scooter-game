e3f8f3991ec75642655304ee4a69a12b57fa225d
krisk <krisk@krisks-MacBook-Pro.local>
2024-06-24 23:03:29 -0700

    added scooter functionality

--- player.gd
+++ player.gd
@@ -1,8 +1,19 @@
 extends RigidBody3D
 
+@onready var car_mesh = $MeshInstance3D
+@onready var ground_ray = $MeshInstance3D/RayCast3D
+var sphere_offset = Vector3(0, -1.0, 0)
+var acceleration = 50
+var steering = 21.0
+var turn_speed = 5
+var turn_stop_limit = 0.75
 
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+func _ready():
+	ground_ray.add_exception(self)
 
-func _physics_process(delta):
-	var input_dir = 0.01*Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	apply_torque_impulse(Vector3(input_dir.y, 0, input_dir.x))
+func _physics_process(_delta):
+	add_constant_central_force(0.05*-car_mesh.global_transform.basis.z * Input.get_axis("down", "up"))
+
+func _process(delta):
+	car_mesh.global_rotation = Vector3.ZERO
+	car_mesh.global_rotation.y = Input.get_axis("right", "left")
