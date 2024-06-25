b8b6611060307ec33aff8bada5e489c40d2ace7a
krisk <krisk@krisks-MacBook-Pro.local>
2024-06-24 23:43:51 -0700

    trying to add better rotation

--- player.gd
+++ player.gd
@@ -1,19 +1,11 @@
 extends RigidBody3D
 
 @onready var car_mesh = $MeshInstance3D
-@onready var ground_ray = $MeshInstance3D/RayCast3D
-var sphere_offset = Vector3(0, -1.0, 0)
-var acceleration = 50
-var steering = 21.0
-var turn_speed = 5
-var turn_stop_limit = 0.75
-
-func _ready():
-	ground_ray.add_exception(self)
 
 func _physics_process(_delta):
 	add_constant_central_force(0.05*-car_mesh.global_transform.basis.z * Input.get_axis("down", "up"))
+	add_constant_torque(0.1*Vector3(0, Input.get_axis("right", "left"), 0))
 
 func _process(delta):
-	car_mesh.global_rotation = Vector3.ZERO
-	car_mesh.global_rotation.y = Input.get_axis("right", "left")
+	car_mesh.global_rotation.x = 0
+	car_mesh.global_rotation.z = 0
