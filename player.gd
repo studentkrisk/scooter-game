392bbb82eef52b96130570d5a5d9be7d3fe19d20
krisk <krisk@krisks-MacBook-Pro.local>
2024-06-25 22:36:28 -0700

    changed back to vehicle body

--- player.gd
+++ player.gd
@@ -1,6 +1,7 @@
-extends CharacterBody3D
+extends VehicleBody3D
 
 func _process(delta):
-	velocity += -transform.basis.z * Input.get_axis("down", "up")
-	velocity *= 0.8
-	move_and_slide()
+	engine_force = Input.get_action_strength("up") * 500
+	brake = Input.get_action_strength("down") * 5
+	steering = move_toward(steering, -Input.get_axis("right", "left"), delta)
+	apply_torque(-100 * rotation.z * Vector3.FORWARD)
