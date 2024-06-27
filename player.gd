2cfc65cbe5b8dc493880a1dbe90e155e632f2b4b
krisk <krisk@krisks-MacBook-Pro.local>
2024-06-26 21:00:04 -0700

    changed to a car and changed lighting

--- player.gd
+++ player.gd
@@ -1,7 +1,6 @@
 extends VehicleBody3D
 
 func _process(delta):
-	engine_force = Input.get_action_strength("up") * 500
-	brake = Input.get_action_strength("down") * 5
-	steering = move_toward(steering, -Input.get_axis("right", "left"), delta)
-	apply_torque(-100 * rotation.z * Vector3.FORWARD)
+	engine_force = Input.get_action_strength("up") * 4000
+	brake = Input.get_action_strength("down") * 50
+	steering = Input.get_axis("right", "left") * 0.1
