2c6b9deda926b009c53c3a6524115e73ff42bfee
krisk <krisk@krisks-MacBook-Pro.local>
2024-06-27 00:24:59 -0700

    stopped car from rolling over and added ramp

--- player.gd
+++ player.gd
@@ -1,6 +1,6 @@
 extends VehicleBody3D
 
 func _process(delta):
-	engine_force = Input.get_action_strength("up") * 4000
+	engine_force = Input.get_action_strength("up") * 8000
 	brake = Input.get_action_strength("down") * 50
 	steering = Input.get_axis("right", "left") * 0.1
