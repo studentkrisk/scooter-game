c5da98c602ed030cfe53fec76912b8c3875861a3
krisk <krisk@krisks-MacBook-Pro.local>
2024-06-30 01:10:45 -0700

    added suburban elements and more city

--- player.gd
+++ player.gd
@@ -3,4 +3,5 @@ extends VehicleBody3D
 func _process(delta):
 	engine_force = Input.get_axis("down", "up") * 12000
 	brake = Input.get_action_strength("down") * 50
-	steering = Input.get_axis("right", "left") * deg_to_rad(20)
+	steering = move_toward(steering, Input.get_axis("right", "left") * deg_to_rad(20), delta)
+	print(steering)
