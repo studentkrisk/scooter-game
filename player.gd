0ad2848174d97462a064176bc568c10011adff3c
krisk <krisk@krisks-MacBook-Pro.local>
2024-06-30 00:07:07 -0700

    added suburban meshes, mapped out city

--- player.gd
+++ player.gd
@@ -1,6 +1,6 @@
 extends VehicleBody3D
 
 func _process(delta):
-	engine_force = Input.get_action_strength("up") * 8000
+	engine_force = Input.get_axis("down", "up") * 12000
 	brake = Input.get_action_strength("down") * 50
-	steering = Input.get_axis("right", "left") * 0.1
+	steering = Input.get_axis("right", "left") * deg_to_rad(20)
