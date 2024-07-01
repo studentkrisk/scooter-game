6a6adb13b493ede1f2d10dbb1010a8f199162e06
krisk <krisk@krisks-MacBook-Pro.local>
2024-06-30 23:35:49 -0700

    added suburbans, started urban

--- player.gd
+++ player.gd
@@ -4,4 +4,4 @@ func _process(delta):
 	engine_force = Input.get_axis("down", "up") * 12000
 	brake = Input.get_action_strength("down") * 50
 	steering = move_toward(steering, Input.get_axis("right", "left") * deg_to_rad(20), delta)
-	print(steering)
+	add_constant_torque(Vector3.FORWARD*Input.get_axis("right", "left"))
